f282916967793ba10146bbbbd418c04202e7e445
Sax Dhiut <mail@iyxeyl.me>
2023-03-03 21:14:48 +0400

    feat: add events_isolation, move stuff around

--- app/util/dispatcher/add_stuff.py
+++ app/util/dispatcher/add_stuff.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.middleware.database_session import get_async_database_session
 from app.middleware.text import filter_non_text
@@ -6,6 +7,9 @@ from app.middleware.user import filter_non_user, update_user
 from app.router.chat import chat_router
 from app.router.management import management_router
 from app.router.start import start_router
+from app.util.crypto.initialize import initialize_fernet
+from app.util.open_ai.chat_gpt import chat_gpt_wrapper
+from app.util.settings import shared_settings
 
 
 def add_routes(dispatcher: Dispatcher) -> Dispatcher:
@@ -21,5 +25,18 @@ def add_message_middleware(dispatcher: Dispatcher) -> Dispatcher:
     dispatcher.message.middleware(filter_non_text)
     dispatcher.message.middleware(get_async_database_session)
     dispatcher.message.middleware(update_user)
+
+    return dispatcher
+
+
+def add_stuff(dispatcher: Dispatcher) -> Dispatcher:
+    dispatcher["async_engine"] = create_async_engine(
+        url=shared_settings.async_database_url,
+        pool_size=shared_settings.database_pool_size,
+        pool_pre_ping=True,
+    )
+
+    dispatcher["chat_prompt"] = chat_gpt_wrapper()
+    dispatcher["fernet"] = initialize_fernet(key=shared_settings.fernet_key_bytes)
 
     return dispatcher
--- app/util/dispatcher/initialize.py
+++ app/util/dispatcher/initialize.py
@@ -1,39 +1,27 @@
 from aiogram import Dispatcher
-from sqlalchemy.ext.asyncio import create_async_engine
+from aiogram.fsm.storage.memory import SimpleEventIsolation
 
 from app.middleware.access import filter_non_allowed
 from app.middleware.admin import filter_non_admin
 from app.router.chat import chat_router
 from app.router.management import management_router
 from app.router.start import start_router
-from app.util.crypto.initialize import initialize_fernet
-from app.util.dispatcher.oof import add_message_middleware, add_routes
+from app.util.dispatcher.add_stuff import add_message_middleware, add_routes, add_stuff
 from app.util.lifecycle.lifecycle_functions import on_shutdown, on_startup
-from app.util.open_ai.chat_gpt import chat_gpt_wrapper
-from app.util.settings import shared_settings
 
 
 def initialize_dispatcher() -> Dispatcher:
-    dispatcher = Dispatcher()
-
-    dispatcher["async_engine"] = create_async_engine(
-        url=shared_settings.async_database_url,
-        pool_size=shared_settings.database_pool_size,
-        pool_pre_ping=True,
-    )
-
-    dispatcher["chat_prompt"] = chat_gpt_wrapper()
-    dispatcher["fernet"] = initialize_fernet(key=shared_settings.fernet_key_bytes)
-
+    dispatcher = Dispatcher(events_isolation=SimpleEventIsolation())
+    dispatcher = add_stuff(dispatcher=dispatcher)
     dispatcher = add_routes(dispatcher=dispatcher)
-
-    dispatcher.startup.register(callback=on_startup)
-    dispatcher.shutdown.register(callback=on_shutdown)
 
     dispatcher = add_message_middleware(dispatcher=dispatcher)
     management_router.message.middleware(filter_non_admin)
 
     start_router.message.middleware(filter_non_allowed)
     chat_router.message.middleware(filter_non_allowed)
+
+    dispatcher.startup.register(callback=on_startup)
+    dispatcher.shutdown.register(callback=on_shutdown)
 
     return dispatcher
