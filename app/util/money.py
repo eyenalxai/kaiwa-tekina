f3a3067c8aff01b8e998d383ba440dcc51da103a
Sax Dhiut <mail@iyxeyl.me>
2023-03-04 00:50:40 +0400

    refactor: round to 2 decimals

--- app/util/money.py
+++ app/util/money.py
@@ -2,4 +2,4 @@ from app.util.settings import shared_settings
 
 
 def tokens_to_usd(tokens: int) -> float:
-    return round(tokens * shared_settings.per_token_price / 1000, 4)
+    return round(tokens * shared_settings.per_token_price / 1000, 2)
